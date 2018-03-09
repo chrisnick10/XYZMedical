6541c25f07ffcb61e7cd4144123999ebeaa1f726
christian <nickolaou@oakland.edu>
2018-03-09 01:26:13 -0500

    the register function pulls a list of usernames from the database, and if there is an existing username then it returns false, else it inserts the new information into the user table

--- src/xyzmedical/controller/RegisterViewController.java
+++ src/xyzmedical/controller/RegisterViewController.java
@@ -5,6 +5,14 @@
  */
 package xyzmedical.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import static xyzmedical.utilities.HttpConnection.HttpConnection;
+
 /**
  *
  * @author christian
@@ -14,9 +22,35 @@ public class RegisterViewController {
     public static boolean register(String userName, String userPassword, char userType) {
         
         String usernameQuery = "SELECT userName FROM USER;";
+        String insertQuery = "INSERT INTO USER (userName, userPassword, userType) VALUES ('" +
+                userName+"','"+userPassword+"','"+userType+"');";
+        System.out.println(insertQuery);
+        String resultString;
+        try {
+            resultString = HttpConnection(usernameQuery);
+            //System.out.println(resultString);
+            
+            
+            JSONArray jsonArr = new JSONArray(resultString);
+            List<String> userList = new ArrayList<String>();
+            for (int i = 0; i < jsonArr.length(); i++) {
+                userList.add(jsonArr.getJSONObject(i).getString("userName"));
+            }
+            
+            for (String s: userList) {
+                if (s != userName) {
+                    String insertString = HttpConnection(insertQuery);
+                    System.out.println(insertString);
+                    return true;
+                }
+            }
+            
+            return false;
+            
+        } catch (Exception ex) {
+            Logger.getLogger(RegisterViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        System.out.println(userName+" "+userPassword+" "+userType);
-        
-        return true;
+        return false;
     }
 }
