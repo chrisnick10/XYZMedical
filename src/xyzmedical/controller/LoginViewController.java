7220cdce7b185de5da78b55b0f9965a51a07abec
Christian Nickolaou <nickolaou@oakland.edu>
2018-03-09 12:23:23 -0500

    clicking login now shows if the username is already in the system

--- src/xyzmedical/controller/LoginViewController.java
+++ src/xyzmedical/controller/LoginViewController.java
@@ -5,6 +5,13 @@
  */
 package xyzmedical.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import static xyzmedical.utilities.HttpConnection.HttpConnection;
+
 /**
  *
  * @author christian
@@ -15,9 +22,36 @@ public class LoginViewController {
     
     public static String login(String userName, String passWord) {
         
-        String loginQuery = "SELECT userPassword FROM USER WHERE";
+        String userQuery = "SELECT userName FROM USER;";
+        String jsonResult;
         
-        return "S";
+        try {
+            
+            jsonResult = HttpConnection(userQuery);
+            JSONArray jsonArr = new JSONArray(jsonResult);
+            List<String> userList = new ArrayList<String>();
+            for (int i = 0; i < jsonArr.length(); i++) {
+                userList.add(jsonArr.getJSONObject(i).getString("userName"));
+            }
+            
+            boolean match = false;
+            for (String user: userList) {
+                if (user.equals(userName)) {
+                    match = true;
+                }
+            }
+            if (!match) {
+                return "NU";
+            }
+            
+        } catch (Exception ex) {
+            Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
+        return "";
     }
     
 }
