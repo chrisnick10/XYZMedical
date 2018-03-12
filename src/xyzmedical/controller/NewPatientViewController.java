97a6138f6991178ae229c2882141c5230856feb0
Christian Nickolaou <nickolaou@oakland.edu>
2018-03-12 11:10:49 -0400

    NewPatientViewController now inserts the new patient data into the database.

--- src/xyzmedical/controller/NewPatientViewController.java
+++ src/xyzmedical/controller/NewPatientViewController.java
@@ -5,10 +5,27 @@
  */
 package xyzmedical.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static xyzmedical.utilities.HttpConnection.HttpConnection;
+
 /**
  *
  * @author cjwn
  */
 public class NewPatientViewController {
     
+    public static void insertNewPatient(String fName, String lName, String dob, String email, String phone, String insurance, int U_ID) {
+        String insertQuery = String.format("INSERT INTO PATIENT (P_ID,PLNAME,PFNAME,DOB,EMAIL,PHONE,INSURANCE,BALANCE,U_ID)VALUES (NULL,'%s','%s','%s',"
+                + "'%s','%s','%s','%d','%d');", lName,fName,dob,email,phone,insurance,0,U_ID);
+        
+        try {
+            String resultString = HttpConnection(insertQuery);
+            System.out.println(insertQuery);
+        } catch (Exception ex) {
+            Logger.getLogger(NewPatientViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+        
+    }
 }
